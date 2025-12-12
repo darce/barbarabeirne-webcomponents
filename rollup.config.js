c966ddb1a7c19fb1bc8fac6226b509ff064b50ad
Daniel Arcé <daniel.arce@gmail.com>
2025-12-11 20:46:25 -0500

    chore: update rollup for component bundling and scss support

--- rollup.config.js
+++ rollup.config.js
@@ -1,22 +1,41 @@
 const path = require('path');
 const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const babel = require('@rollup/plugin-babel').default;
+const scss = require('rollup-plugin-scss');
 
-module.exports = {
-    input: path.resolve(__dirname, 'src/init.js'),
-    output: {
-        file: path.resolve(__dirname, 'lib/init.js'),
-        format: 'iife',
-        name: 'GalleryInit',
-        sourcemap: true,
+const plugins = [
+    nodeResolve({ browser: true }),
+    babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+    }),
+];
+
+module.exports = [
+    {
+        input: path.resolve(__dirname, 'src/init.js'),
+        output: {
+            file: path.resolve(__dirname, 'lib/init.js'),
+            format: 'iife',
+            name: 'GalleryInit',
+            sourcemap: true,
+        },
+        plugins,
     },
-    plugins: [
-        nodeResolve({
-            browser: true,
-        }),
-        babel({
-            babelHelpers: 'bundled',
-            exclude: 'node_modules/**',
-        }),
-    ],
-};
+    {
+        input: path.resolve(__dirname, 'src/components/dax-carousel/dax-carousel.js'),
+        output: {
+            file: path.resolve(__dirname, 'lib/components/dax-carousel/dax-carousel.js'),
+            format: 'esm',
+            sourcemap: true,
+        },
+        plugins: [
+            nodeResolve({ browser: true }),
+            scss({ output: false }),
+            babel({
+                babelHelpers: 'bundled',
+                exclude: 'node_modules/**',
+            }),
+        ],
+    },
+];
